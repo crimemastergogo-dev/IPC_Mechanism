7f9bd5ae3cbcb3772cfc6ba076e80928623f3405
satvik <satvik307@gmail.com>
2020-04-01 10:09:34 +0530

    Message queue intro

---
+++ message_queue/msgR.c
@@ -0,0 +1,12 @@
+/* Made by - Satvik Sharma
+ * Last modified on : 1/4/2020*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+/**/
+#include<sys/msg.h>
+ 
+
